de5f181a7293d8623c0cf1c03bed107435b3e9b8
MINGU <mgseo@adenasoft.com>
2026-02-15 00:52:01 +0900

    chore: 코드 위치 변경

--- CrashlyticsReport/AppView2.swift
+++ CrashlyticsReport/AppView2.swift
@@ -21,29 +21,35 @@ struct AppView2: View {
     let error = NSError()
     let array = ["a", "b", "c"]
     
+    
+    
     var body: some View {
         VStack {
             
             
+           
             Button("FatalCrash") {
               fatalError("Crash was triggered")
             }
+            
             Button("FatalErrorCrash") {
               fatalError("Crash was triggered")
             }
-            
             
             Button("IndexCrash") {
               print(array[4])
             }
             
+            
             Button("IndexCrash2") {
               print(array[4])
             }
             
+            
             Button("IndexCrash3") {
               print(array[4])
             }
+            
             
             Button("CrashCustomError") {
                 Crashlytics.crashlytics().record(error: error)
